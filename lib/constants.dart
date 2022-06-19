259999640a9d96212a7ad63bba665d3d9a0fa87d
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-06-18 21:50:42 -0300

    primeiro commit do app de imc

---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+class Constants {
+  static const String title = 'Calculador de IMC';
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:imc_mobile_app/constants.dart';
+import 'package:imc_mobile_app/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: Constants.title,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: const HomeScreen(title: Constants.title),
+    );
+  }
+}
